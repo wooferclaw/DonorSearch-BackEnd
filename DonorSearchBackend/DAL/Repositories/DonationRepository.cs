840f02888b7072c85ffad20ce0c650d65db1681a
Elena Selivanova <chiffa44@gmail.com>
2018-11-11 03:35:28 +0300

    [Donations]:Update

--- DonorSearchBackend/DAL/Repositories/DonationRepository.cs
+++ DonorSearchBackend/DAL/Repositories/DonationRepository.cs
@@ -22,15 +22,49 @@ namespace DonorSearchBackend.DAL.Repositories
         {
             using (ApplicationContext db = new ApplicationContext())
             {
+                donation.confirm_visit = new ValidationVisit();
+                //если заполнена дата донации, то определим период для повторного визита 
+                //и дату когда будут посылаться уведомления с рекомендацией
+                if (donation.donation_date.HasValue)
+                {
+                    CalculateDates(donation);
+                }
                 db.Donations.Add(donation);
                 db.SaveChanges();
             }
         }
+
+        private static void CalculateDates(Donation donation)
+        {
+            donation.confirm_visit.date_from = donation.donation_date.Value.AddMonths(3);
+            donation.confirm_visit.date_to = donation.donation_date.Value.AddMonths(6);
+            //напоминать за 3,2,1 день (в зависимости от того, сколько дней осталось)
+            double days = (donation.donation_date.Value.Subtract(DateTime.Now)).TotalDays;
+            if (days >= 3)
+            {
+                donation.recomendation_timestamp = donation.donation_date.Value.AddDays(-3);
+            }
+            else if (days < 3 && days >= 2)
+            {
+                donation.recomendation_timestamp = donation.donation_date.Value.AddDays(-2);
+            }
+            else if (days < 2)
+            {
+                donation.recomendation_timestamp = donation.donation_date.Value.AddDays(-1);
+            }
+        }
+
         public static void UpdateDonation(Donation donation)
         {
             using (ApplicationContext db = new ApplicationContext())
             {
                 var originalDonation= db.Donations.FirstOrDefault(d => d.id == donation.id && d.vk_id == donation.vk_id);
+                //если заполнена дата донации+она изменилась, то определим период для повторного визита 
+                //и дату когда будут посылаться уведомления с рекомендацией
+                if(donation.donation_date.HasValue && donation.donation_date != originalDonation.donation_date)
+                {
+                    CalculateDates(donation);
+                }
                 db.Entry(originalDonation).CurrentValues.SetValues(donation);
                 db.SaveChanges();
             }
